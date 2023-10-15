dee1d1234ae61c73484f7d7bc3c2ec8c1b502b9d
Seacific <66648696+Seacific@users.noreply.github.com>
2023-10-14 22:45:24 -0400

    finished triangle function

--- src/main.c
+++ src/main.c
@@ -23,12 +23,14 @@ bool quit = false;
 void drawRectangle(SDL_Surface* surface, vec2 pos, u32 width, u32 height);
 
 void drawPixel(SDL_Surface* surface, ivec2 A);
-void drawTriangle(SDL_Surface* surface, vec3* vertices, int X, int Y);
-void tracePixels(SDL_Surface* surface);
+bool isIntersectingTriangle(SDL_Surface* surface, vec3* vertices, int X, int Y);
+void tracePixels(SDL_Surface* surface, vec3* vertices);
 
 vec3* calculateNormalizedRayVectors();
 
 vec3* normVectors;
+
+vec3 cameraPos = {0,0,0};
 
 int main(int argc, char* args[]){
 
@@ -57,7 +59,7 @@ int main(int argc, char* args[]){
 
 	while (!quit) {
 		
-        tracePixels(surface);
+       // tracePixels(surface, vertices);
 
 		SDL_UpdateWindowSurface(window); SDL_Event e;
 		
@@ -72,26 +74,58 @@ int main(int argc, char* args[]){
 
 }
 
-void tracePixels(SDL_Surface* surface) {
+void tracePixels(SDL_Surface* surface, vec3* vertices) {
     for (int X = 0; X < WIDTH; X++) {
         for (int Y = 0; Y < HEIGHT; Y++) {
-            
+            if (isIntersectingTriangle(surface, vertices, X, Y)) {
+                drawPixel(surface, (ivec2){X, Y}); 
+            }
         }
     }
 }
-void drawTriangle(SDL_Surface* surface, vec3* vertices, int X, int Y) {
-   vec3 AB, AC, n;
+
+// Most of the math is stolen from this pdf: https://courses.cs.washington.edu/courses/csep557/10au/lectures/triangle_intersection.pdf
+bool isIntersectingTriangle(SDL_Surface* surface, vec3* vertices, int X, int Y) {
+   vec3 AB, AC, BC, CA, n, cross;
    glm_vec3_sub(vertices[0], vertices[1], AB);
    glm_vec3_sub(vertices[0], vertices[2], AC);
+   glm_vec3_sub(vertices[1], vertices[2], BC);
+   glm_vec3_sub(vertices[2], vertices[0], CA);
 
    glm_vec3_crossn(AB, AC, n);
    
    int d = glm_vec3_dot(n, vertices[0]);
 
-   vec3 D;
+   vec3 D, Q;
 
    memcpy(&D, normVectors[X*Y], sizeof(vec3));
 
+   int t = (d-(glm_dot(n, cameraPos)))/(glm_dot(n,D));
+
+   glm_vec3_scale(D, t, Q); 
+   glm_vec3_add(Q, cameraPos, Q);
+
+   vec3 AQ, BQ, CQ;
+   glm_vec3_sub(Q, vertices[0], AQ);
+   glm_vec3_sub(Q, vertices[1], BQ);
+   glm_vec3_sub(Q, vertices[2], CQ);
+
+   glm_vec3_cross(AB, AQ, cross);
+   if (glm_dot(cross, n) < 0) {
+        return 0;
+   }
+
+   glm_vec3_cross(BC, BQ, cross);
+   if (glm_dot(cross, n) < 0) {
+        return 0;
+   }
+
+   glm_vec3_cross(CA, CQ, cross);
+   if (glm_dot(cross, n) < 0) {
+        return 0;
+   }
+
+   return 1;
 }
 
 void drawPixel(SDL_Surface* surface, ivec2 A) {
