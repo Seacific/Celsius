8303970ede771ed64aa43f9c9ee4b9f51a6b5e78
Seacific <66648696+Seacific@users.noreply.github.com>
2024-01-30 11:04:42 -0500

    Updated Files

--- Makefile
+++
@@ -1,3 +0,0 @@
-RaycastEngine: src/main.c
-	gcc src/main.c -LDependencies/lib/ -lSDL2 -o RaycastRenderer -IDependencies/include/ -g
-	./RaycastRenderer
--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <cglm/cglm.h>
+#include "celsius.h"
 
 typedef float    f32;
 typedef double   f64;
@@ -18,21 +19,24 @@ typedef int64_t  i64;
 
 u32 framebufferArray[WIDTH * HEIGHT] = {0};
 
+bool debugged = false;
+
 bool quit = false;
 
 void drawRectangle(SDL_Surface* surface, vec2 pos, u32 width, u32 height);
 
 void drawPixel(SDL_Surface* surface, ivec2 A);
-bool isIntersectingTriangle(SDL_Surface* surface, vec3* vertices, int X, int Y);
-void tracePixels(SDL_Surface* surface, vec3* vertices);
+float isIntersectingTriangle(Celsius_Renderer* renderer, vec3* vertices, int X, int Y);
+void tracePixels(Celsius_Renderer* renderer, vec3* vertices);
+void printVector(vec3 vector);
+void clear(Celsius_Renderer* renderer);
 
 vec3* calculateNormalizedRayVectors();
 
-vec3* normVectors;
-
-vec3 cameraPos = {0,0,0};
 
 int main(int argc, char* args[]){
+
+    freopen("debug.out", "w", stdout);
 
 	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
     	{
@@ -46,82 +50,106 @@ int main(int argc, char* args[]){
 	}
 
 
-
-	SDL_Surface* surface = SDL_GetWindowSurface(window);
+    Celsius_Renderer* renderer = Celsius_CreateRenderer(
+            window, 
+            Celsius_CreateCamera(WIDTH, HEIGHT, (vec3){0, 0, 0}, calculateNormalizedRayVectors()),
+            WIDTH,
+            HEIGHT
+            );
+    
 
 	vec3 vertices[3] = {
-		{-0.5f, -0.5f, 3.0f},
-		{0.0f, 0.5f, 3.0f},
-		{0.5f, -0.5f, 3.0f}
+		{-2.0f, -1.0f, 3.0f},
+		{2.0f, -1.0f, 3.0f},
+		{0.0f, 1.0f, 3.0f}
 	};
 
-    vec3* normVectors = calculateNormalizedRayVectors();
+
+    /*for (int X = 0; X < WIDTH; X++) {
+        for (int Y = 0; Y < HEIGHT; Y++) {
+            printf("%f ", normVectors[X*Y][
+        }
+    }*/
+
 
 	while (!quit) {
-		
-       // tracePixels(surface, vertices);
 
 		SDL_UpdateWindowSurface(window); SDL_Event e;
-		
+        Celsius_Clear(renderer);
+        tracePixels(renderer, vertices);
+    printf("%f", isIntersectingTriangle(renderer, vertices, WIDTH/2, HEIGHT/2));		
 		while(SDL_PollEvent(&e) > 0){
 			switch (e.type) {
 				case SDL_QUIT:
 					quit = true;
 					break;
+                case SDL_KEYDOWN:
+                    switch (e.key.keysym.sym) {
+                        case SDLK_UP:
+                            renderer->camera->position[2] += 0.1;
+                            break;
+                        case SDLK_DOWN:
+                            renderer->camera->position[2] -= 0.1;
+                            break;
+                    }
 			}
 		}
 	}
 
 }
 
-void tracePixels(SDL_Surface* surface, vec3* vertices) {
+void tracePixels(Celsius_Renderer* renderer, vec3* vertices) {
     for (int X = 0; X < WIDTH; X++) {
         for (int Y = 0; Y < HEIGHT; Y++) {
-            if (isIntersectingTriangle(surface, vertices, X, Y)) {
-                drawPixel(surface, (ivec2){X, Y}); 
-            }
+          if (isIntersectingTriangle(renderer, vertices, X, Y)){
+            drawPixel(renderer->surface, (ivec2){X, Y}); 
+            debugged = true;
+        }
         }
     }
 }
 
 // Most of the math is stolen from this pdf: https://courses.cs.washington.edu/courses/csep557/10au/lectures/triangle_intersection.pdf
-bool isIntersectingTriangle(SDL_Surface* surface, vec3* vertices, int X, int Y) {
+float isIntersectingTriangle(Celsius_Renderer* renderer, vec3* vertices, int X, int Y) {
    vec3 AB, AC, BC, CA, n, cross;
-   glm_vec3_sub(vertices[0], vertices[1], AB);
-   glm_vec3_sub(vertices[0], vertices[2], AC);
-   glm_vec3_sub(vertices[1], vertices[2], BC);
-   glm_vec3_sub(vertices[2], vertices[0], CA);
+
+   glm_vec3_sub(vertices[1], vertices[0], AB);
+   glm_vec3_sub(vertices[2], vertices[0], AC);
+   glm_vec3_sub(vertices[2], vertices[1], BC);
+   glm_vec3_sub(vertices[0], vertices[2], CA);
 
    glm_vec3_crossn(AB, AC, n);
-   
-   int d = glm_vec3_dot(n, vertices[0]);
 
    vec3 D, Q;
 
-   memcpy(&D, normVectors[X*Y], sizeof(vec3));
+   memcpy(&D, renderer->camera->pixelRays[Y*WIDTH+X], sizeof(vec3));
 
-   int t = (d-(glm_dot(n, cameraPos)))/(glm_dot(n,D));
+   float d = glm_vec3_dot(n, vertices[0]);
+   float t = (d-(glm_vec3_dot(n, renderer->camera->position)))/(glm_vec3_dot(n, D));
 
-   glm_vec3_scale(D, t, Q); 
-   glm_vec3_add(Q, cameraPos, Q);
+   vec3 td;
+   glm_vec3_scale(D, t, td);
+   glm_vec3_add(renderer->camera->position, td, Q);
 
    vec3 AQ, BQ, CQ;
+   vec3 testVector;
    glm_vec3_sub(Q, vertices[0], AQ);
    glm_vec3_sub(Q, vertices[1], BQ);
    glm_vec3_sub(Q, vertices[2], CQ);
+   
+   glm_vec3_cross(AB, AQ, testVector);
 
-   glm_vec3_cross(AB, AQ, cross);
-   if (glm_dot(cross, n) < 0) {
+   if (glm_dot(testVector, n) < 0) {
         return 0;
    }
+   glm_vec3_cross(BC, BQ, testVector);
 
-   glm_vec3_cross(BC, BQ, cross);
-   if (glm_dot(cross, n) < 0) {
+   if (glm_dot(testVector, n) < 0) {
         return 0;
    }
+   glm_vec3_cross(CA, CQ, testVector);
 
-   glm_vec3_cross(CA, CQ, cross);
-   if (glm_dot(cross, n) < 0) {
+   if (glm_dot(testVector, n) < 0) {
         return 0;
    }
 
@@ -130,7 +158,9 @@ bool isIntersectingTriangle(SDL_Surface* surface, vec3* vertices, int X, int Y) 
 
 void drawPixel(SDL_Surface* surface, ivec2 A) {
 
-		((u32*)surface->pixels)[A[0]+ WIDTH*A[1]] = 0xFFFFFFFF;
+		((u8*)surface->pixels)[(A[0]+ WIDTH*A[1])*4] = 0xFF;
+		((u8*)surface->pixels)[(A[0]+ WIDTH*A[1])*4 + 1] = 0xFF;
+		((u8*)surface->pixels)[(A[0]+ WIDTH*A[1])*4 + 2] = 0xFF;
 }
 
 u32* getPixel(SDL_Surface* surface, ivec2 A) {
@@ -141,9 +171,9 @@ vec3* calculateNormalizedRayVectors() {
      vec3* table = malloc(sizeof(vec3)*WIDTH*HEIGHT);
      for (int Y = 0; Y < HEIGHT; Y++) {
         for (int X = 0; X < WIDTH; X++) {
-            table[X+Y][0] = X-(WIDTH/2);
-            table[X+Y][1] = Y-(HEIGHT/2);
-            table[X+Y][2] = 1;
+            table[Y*WIDTH+X][0] = X-(WIDTH/2);
+            table[Y*WIDTH+X][1] = (HEIGHT/2)-Y;
+            table[Y*WIDTH+X][2] = 100;
         }
      }
      for (int i = 0; i < WIDTH*HEIGHT; i++) {
@@ -151,3 +181,12 @@ vec3* calculateNormalizedRayVectors() {
      }
      return table;
 }
+
+void printVector(vec3 vector) {
+    for (int i = 0; i < 3; i++) {
+        printf("%f \n",vector[i]);
+    }
+}
+
+void clear(Celsius_Renderer* renderer) {
+    }
